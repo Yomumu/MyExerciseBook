60b71692048a971b363c38d609f48f4d385d9918
Yomumu <1101272655@qq.com>
2014-08-05 21:03:50 +0800

    测试使用

---
+++ Xpath/GitTest/src/com/sangmu/cn/Demo.java
@@ -0,0 +1,18 @@
+package com.sangmu.cn;
+
+public class Demo {
+
+	public static void main(String[] args) {
+        fun1(fun2());
+        System.out.println("≤‚ ‘£°");
+	}
+
+	public static void fun1(int a) {
+        System.out.println(a);
+	}
+
+	public static int fun2() {
+       return 3;
+	}
+
+}
